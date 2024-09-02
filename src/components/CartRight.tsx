5599a70b08bbabb2d313e0a0681dfbb54c11533a
Rian Bittencourt <109157383+rianbittencourt@users.noreply.github.com>
2024-09-02 13:45:41 -0300

    refactor

--- src/components/CartRight.tsx
+++ src/components/CartRight.tsx
@@ -6,14 +6,51 @@ import { OpenCartAtom } from "@/atom/atom";
 import { useEffect } from "react";
 import { cartAtom } from "@/atom/cartAtom";
 import { IoTrashOutline } from "react-icons/io5";
+import { removeFromCart } from "@/atom/cartAtom";
+import IconEmptyCart from "./Icons/IconEmptyCart";
 
 export default function CartRight() {
   const [open, setOpen] = useAtom(OpenCartAtom);
   const [cart] = useAtom(cartAtom);
+  const [, remove] = useAtom(removeFromCart);
 
   useEffect(() => {
     console.log("teste", cart);
   });
+
+  function ProductCardCard() {
+    return (
+      <div className="flex flex-col gap-2 ">
+        {cart.map((product) => (
+          <div key={product.sku} className="flex gap-2 ">
+            <div className="h-20  bg-gray-100 p-1 w-[50%]  ">
+              <img src={product.image} alt="sneaker" />
+            </div>
+            <div className="w-full">
+              <h1 className="text-sm font-light">{product.name}</h1>
+              <p className="text-sm font-light ">${product.price}</p>
+              <p className="text-sm font-light ">Size: L</p>
+            </div>
+            <div className="flex justify-end w-full">
+              <IoTrashOutline
+                className="cursor-pointer hover:text-red-500 duration-100 hover:scale-125 text-lg"
+                onClick={() => remove(product.sku)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  function EmptyCart() {
+    return (
+      <div className="flex justify-center flex-col items-center">
+        <IconEmptyCart />
+        <p className="text-sm text-stone-600"> Your cart is currently empty.</p>
+      </div>
+    );
+  }
 
   return (
     <Dialog open={open} onClose={setOpen} className="relative z-[9000]">
@@ -47,56 +84,38 @@ export default function CartRight() {
                     </div>
                   </div>
                   <div className="relative mt-6 flex-1 px-4 sm:px-6 ">
-                    <div className="flex flex-col gap-2 ">
-                      {cart.map((product) => (
-                        <div key={product.sku} className="flex gap-2 ">
-                          <div className="h-20  bg-gray-100 p-1 w-[50%]  ">
-                            <img src={product.image} alt="sneaker" />
-                          </div>
-                          <div className="w-full">
-                            <h1 className="text-sm font-light">
-                              {product.name}
-                            </h1>
-                            <p className="text-sm font-light ">
-                              ${product.price}
-                            </p>
-                            <p className="text-sm font-light ">Size: L</p>
-                          </div>
-                          <div className="flex justify-end w-full">
-                            <IoTrashOutline />
-                          </div>
-                        </div>
-                      ))}
-                    </div>
+                    {cart.length > 0 ? <ProductCardCard /> : <EmptyCart />}
                   </div>
                 </div>
-                <div className="flex flex-col gap-2 flex-shrink-0 justify-end px-4 py-4 bg-gray-100">
-                  <div className="flex justify-between px-2">
-                    <div>
-                      <p>Total Item</p>
-                      <p className="font-bold">{cart.length}</p>
+                {cart.length > 0 && (
+                  <div className="flex flex-col gap-2 flex-shrink-0 justify-end px-4 py-4 bg-gray-100">
+                    <div className="flex justify-between px-2">
+                      <div>
+                        <p>Total Item</p>
+                        <p className="font-bold">{cart.length}</p>
+                      </div>
+                      <div>
+                        <p>Subtotal</p>
+                        <p className="font-bold">
+                          ${cart.reduce((acc, item) => acc + item.price, 0)}
+                        </p>
+                      </div>
                     </div>
-                    <div>
-                      <p>Subtotal</p>
-                      <p className="font-bold">
-                        ${cart.reduce((acc, item) => acc + item.price, 0)}
-                      </p>
-                    </div>
+                    <button
+                      type="button"
+                      onClick={() => setOpen(false)}
+                      className="rounded-md bg-stone-900 px-3 py-4 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400"
+                    >
+                      View Cart
+                    </button>
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-4 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    >
+                      Chekout
+                    </button>
                   </div>
-                  <button
-                    type="button"
-                    onClick={() => setOpen(false)}
-                    className="rounded-md bg-stone-900 px-3 py-4 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400"
-                  >
-                    View Cart
-                  </button>
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-4 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    Chekout
-                  </button>
-                </div>
+                )}
               </div>
             </DialogPanel>
           </div>
