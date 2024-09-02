fc85edbff8c78a462f1959df1585b403414b2ae0
Rian Bittencourt <109157383+rianbittencourt@users.noreply.github.com>
2024-09-02 13:46:07 -0300

    refactor

--- src/components/Header/CartButton.tsx
+++ src/components/Header/CartButton.tsx
@@ -6,15 +6,13 @@ import { useAtom } from "jotai";
 
 export default function CartButton() {
   const [cart] = useAtom(cartAtom);
-  const [open, setOpen] = useAtom(OpenCartAtom);
-
-  function setOpenModal() {
-    setOpen(true);
-    console.log(open);
-  }
+  const [, setOpen] = useAtom(OpenCartAtom);
 
   return (
-    <div className="relative hover:scale-105 " onClick={() => setOpenModal()}>
+    <div
+      className="relative hover:scale-105 cursor-pointer select-none "
+      onClick={() => setOpen(true)}
+    >
       <BsBag className="text-white text-2xl" />
       <div className="bg-white absolute -right-2 -top-1 rounded-full h-5 w-5 flex justify-center items-center font-semibold text-stone-500 text-sm  ">
         {cart.length}
